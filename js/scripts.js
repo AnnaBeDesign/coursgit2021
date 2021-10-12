ea5061f5e1c6755af05d457b1137431a1acae66e
Annabelle Ndiho <ndihoannabelle6@gmail.com>
2021-10-12 11:08:36 +0200

    tout envoyer

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    
+    $("button:nth-child(first)").click(function(){
+        $(".femme").hide();
+      });
+
+
+      $("button:nth-child(2)").click(function(){
+        $(".homme").hide();
+      });
+
+
+
+
+    });
